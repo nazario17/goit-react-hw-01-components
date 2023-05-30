6f850bb97a9f544d484572f90d334acc77802803
nazario17 <hazarkorchak@gmail.com>
2023-05-31 02:30:40 +0300

    task-4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Profile } from 'components/Profile/profile';
 import { Statistics } from 'components/Statistics/statistic';
 import { FriendList } from 'components/Friends/friendList';
+import { Transaction } from 'components/Transaction/transaction';
 
 import user from '../Profile/user.json';
 import data from '../Statistics/data.json';
 import friends from '../Friends/friends.json';
+import transactions from '../Transaction/transactions.json';
 
 export const App = () => {
   return (
@@ -19,6 +21,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <Transaction items={transactions} />
     </div>
   );
 };
---
+++ src/components/Transaction/transaction.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './transaction.module.css';
+
+export const Transaction = ({ items }) => {
+  return (
+    <table className={css.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item, idx) => (
+          <tr key={item.id} className={idx % 2 ? css.evenRow : css.oddRow}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
