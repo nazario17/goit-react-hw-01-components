f4812205f78d709670d74341fd46743ff10f94d2
nazario17 <hazarkorchak@gmail.com>
2023-05-31 02:40:35 +0300

    fix task-4

--- src/components/Transaction/transaction.jsx
+++ src/components/Transaction/transaction.jsx
@@ -24,3 +24,15 @@ export const Transaction = ({ items }) => {
     </table>
   );
 };
+
+
+Transaction.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
